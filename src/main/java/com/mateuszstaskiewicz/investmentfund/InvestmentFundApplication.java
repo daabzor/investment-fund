81dc552cf31bf550de71f4db9954c384d2f1b2ec
Mateusz Staskiewicz <mateuszstaskiewicz@gmail.com>
2018-11-22 22:08:14 +0100

    Initial commit

---
+++ src/main/java/com/mateuszstaskiewicz/investmentfund/InvestmentFundApplication.java
@@ -0,0 +1,12 @@
+package com.mateuszstaskiewicz.investmentfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentFundApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mateuszstaskiewicz/investmentfund/InvestmentFundApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mateuszstaskiewicz.investmentfund;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvestmentFundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
